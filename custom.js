a3656e1e4e8d9540516f82a4727241523b8105e0
Mehmet KARAKÖSE <mhtkarakose@gmail.com>
2017-12-13 09:50:18 +0300

    revised
    content filling with web api

---
+++ custom.js
@@ -0,0 +1,51 @@
+var Data;
+$.ajax({
+
+    url: "https://www.netdata.com/JSON/aed8665a/",
+    method: "GET",
+    data: Data,
+
+}).success(function (Data) {
+    //console.log("successfully run ajax request..." + Data);
+
+    for(var i=0; i<20; i++){
+
+      // url parsing start
+      var parser = document.createElement('a');
+      parser.href = Data[i].dc_Link;
+      var pro = parser.protocol;
+      var youtube = parser.host;
+      var str = parser.search;
+      var res = str.slice(3,14);
+      // url parsing finish
+
+      // creating url
+      var link = pro + "//" + youtube + "/embed/" + res;
+
+      // creating video name
+      var VideoName = Data[i].dc_Video_Baslik;
+
+      // creating video playlist
+      var VideoPlaylist = Data[i].dc_Playlist_Kategori;
+
+      // creating video detail
+      var VideoRank = Data[i].dc_Izlenme_Sayisi;
+
+      // display video in html
+      $("#Content").append('<div class="col contentCard"><iframe class="contentIframe" src="' + link + '"></iframe><h4 class="contentTitle">'+ VideoName +'</h4><p class=" contentDescription">' + VideoPlaylist + '</p><p class="contentRankVideo">' + VideoRank + ' görüntülüme</p></div>');
+
+    }
+
+}).done(function () {
+
+    console.log("I am from done function");
+
+}).fail(function () {
+
+    console.log("I am from fail function.");
+
+}).always(function () {
+
+    console.log("I am from always function");
+
+});
